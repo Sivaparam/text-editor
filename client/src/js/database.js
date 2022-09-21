89102fb3d512af6123b778b86175c368aa6fbef7
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-09-21 15:26:54 -0400

    added getall and put method in database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  //creates connection to database and the version
+ const textEditorDb = await openDB('jate', 1);
+ // create new transaction and specify the database and data privilages
+ const transaction = textEditorDb.transaction('jate', 'readwrite');
+ // open up desired object store
+ const store = transaction.objectStore('jate');
+ // add content to databse
+ const request = store.put({id: 1, value: content});
+ //get confirmation of result
+ const result = await request;
+
+  console.log('Data saved to database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  //create a connection to the database and specify the version to use
+  const textEditorDb = await openDB('jate', 1);
+  //create a new transaction and specify the database and data privilages
+  const transaction = textEditorDb.transaction('jate', 'readonly');
+  //open up the desired object store
+  const store = transaction.objectStore('jate');
+  //getAll() method to get all data from database
+  const request = store.getAll();
+
+//get confirmation of the result
+const result = await request;
+console.log('result.value', result);
+}
 
 initdb();
